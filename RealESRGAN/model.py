d894fce1c6a17a5a282718dd51b563613f6fd6b7
Doug Haber <dhaber@leshylabs.com>
2024-10-20 23:00:59 -0400

    Use torch.amp.autocast() instead of deprecated style

--- RealESRGAN/model.py
+++ RealESRGAN/model.py
@@ -56,7 +56,7 @@ class RealESRGAN:
         self.model.eval()
         self.model.to(self.device)
         
-    @torch.cuda.amp.autocast()
+    @torch.amp.autocast(device_type='cuda')
     def predict(self, lr_image, batch_size=4, patches_size=192,
                 padding=24, pad_size=15):
         scale = self.scale
